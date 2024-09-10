364fd4e4190adca8da54f48ef97e744b0a1eb2a7
Hanna Voronova <amoramio@gmail.com>
2024-09-10 11:27:26 -0400

    customers api tests: smoke, fixture

--- src/api/service/customers.service.ts
+++ src/api/service/customers.service.ts
@@ -17,4 +17,31 @@ export class CustomersApiService {
 		validateResponse(response, STATUS_CODES.CREATED, true, null);
 		return response.body.Customer;
 	}
+
+	async viewAllCustomers() {
+		const response = await this.customersClient.getAllCustomers(await signInApi.getToken());
+		validateResponse(response, STATUS_CODES.OK, true, null);
+		return response.body.Customers;
+	}
+
+	async viewCustomerById(id: string) {
+		const response = await this.customersClient.viewCustomerbyId(id, await signInApi.getToken());
+		validateResponse(response, STATUS_CODES.OK, true, null);
+		return response.body.Customer;
+	}
+
+	async delete(id: string) {
+		const response = await this.customersClient.deleteCustomerbyId(id, await signInApi.getToken());
+		return response;
+	}
+
+	async updatedCustomerByID(id: string, data: ICustomer) {
+		const response = await this.customersClient.updateCustomerById(
+			id,
+			data,
+			await signInApi.getToken()
+		);
+		validateResponse(response, STATUS_CODES.OK, true, null);
+		return response;
+	}
 }
